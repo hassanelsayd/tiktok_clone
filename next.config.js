2adef550a705b803e43e291d2d3dfdb5146f996f
hassanelsayd <hassan22elsayd@gmail.com>
2022-09-01 15:25:19 +0200

    update config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  typescript: { ignoreBuildErrors },
   swcMinify: true,
   images: {
     domains: ["pps.whatsapp.net", "lh3.googleusercontent.com"],
